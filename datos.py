6ae6df9e33149f4dfb469f20757068bc7e63fb38
mateo <115825282+mateo423@users.noreply.github.com>
2023-06-03 15:11:30 -0500

    Agregando Datos de requests

---
+++ datos.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.frases.net/frases-romanticas-para-enamorar-71.html"
+
+pagina = requests.get(url)
+
+bs  = BeautifulSoup(pagina.content,'html.parser')
+
+
+mensaje = bs.find_all('div',class_='quote_text')
+
+texto = open('mensaje.txt','w')
+
+for i in mensaje:
+    texto.write(i.text)
+
+texto.close()
