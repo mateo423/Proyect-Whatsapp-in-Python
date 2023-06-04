7e1d8e141fe2930e48b2c8b70357b239e7c77d89
mateo423 <elpoderosomateo@gmail.com>
2023-06-03 21:18:48 -0500

    Python-WhatsApp

--- Whatsapp.py
+++ Whatsapp.py
@@ -3,20 +3,20 @@ import time
 import pyautogui
 import webbrowser
 
-numero_telefono = '#'
+numero_telefono = '57 321 9963163'
 
 msm = open('./mensaje.txt')
 
 mensaje = []
 for i in msm:
-    if i !=  '\n':
+    if i != '\n':
         mensaje.append(i)
 
 print(len(mensaje))
 
-n = numpy.random.randint(0,83)
+n = numpy.random.randint(0, 83)
 
-url = 'https://web.whatsapp.com/'
+url = 'https://web.whatsapp.com/send?phone='
 
 webbrowser.open(url + numero_telefono)
 time.sleep(10)
--- datos.py
+++ datos.py
@@ -5,12 +5,12 @@ url = "https://www.frasess.net/frases-romanticas-para-enamorar-71.html"
 
 pagina = requests.get(url)
 
-bs  = BeautifulSoup(pagina.content,'html.parser')
+bs = BeautifulSoup(pagina.content, 'html.parser')
 
 
-mensaje = bs.find_all('div',class_='quote_text')
+mensaje = bs.find_all('div', class_='quote_text')
 
-texto = open('mensaje.txt','w')
+texto = open('mensaje.txt', 'w')
 
 for i in mensaje:
     texto.write(i.text)
